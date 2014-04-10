a46fa3e2a09779d6ecfc16f998e1b3fc3b7027e2
Kyler Tolleson <kylertoll@yahoo.com>
2014-04-10 09:06:35 -0600

    changed the images into a horizontal list

--- menu/animated-menu.js
+++ menu/animated-menu.js
@@ -8,12 +8,12 @@ $(document).ready(function(){
 	});
 	
 	//When mouse rolls over
-	$("li").mouseover(function(){
+	$("#menu li").mouseover(function(){
 		$(this).stop().animate({height:'150px'},{queue:false, duration:600, easing: 'easeOutBounce'})
 	});
 	
 	//When mouse is removed
-	$("li").mouseout(function(){
+	$("#menu li").mouseout(function(){
 		$(this).stop().animate({height:'50px'},{queue:false, duration:600, easing: 'easeOutBounce'})
 	});
 	
